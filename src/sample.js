fe8918bba895e29289fa463676b26b0dd8c1d2fe
ashankaushalya97 <ashankaushalya952@gmail.com>
2020-02-12 15:26:07 +0530

    Add antD framework

--- src/sample.js
+++ src/sample.js
@@ -3,14 +3,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
-import App from "./App"
 import { Layout, Menu, Breadcrumb } from 'antd';
 
 const { Header, Content, Footer } = Layout;
 
-ReactDOM.render(
-  <Layout>
-    <Header style={{ position: 'fixed', zIndex: 1, width: '100%' }}>
+function sample(){
+    return(
+        <Layout className="layout">
+    <Header>
       <div className="logo" />
       <Menu
         theme="dark"
@@ -23,18 +23,17 @@ ReactDOM.render(
         <Menu.Item key="3">nav 3</Menu.Item>
       </Menu>
     </Header>
-    <Content style={{ padding: '0 50px', marginTop: 64 }}>
+    <Content style={{ padding: '0 50px' }}>
       <Breadcrumb style={{ margin: '16px 0' }}>
         <Breadcrumb.Item>Home</Breadcrumb.Item>
         <Breadcrumb.Item>List</Breadcrumb.Item>
         <Breadcrumb.Item>App</Breadcrumb.Item>
       </Breadcrumb>
-      <div style={{ background: '#fff', padding: 24, minHeight: 650 }}>Content
-      <App/>
-      </div>
+      <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
     </Content>
-    {/* <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer> */}
-  </Layout>,
-  document.getElementById('container'),
-);
-          
+    <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+  </Layout>
+    )
+}
+
+export default sample;
