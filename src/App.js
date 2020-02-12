71c252338f14626eb9f669b49c540a6003ccb066
ashankaushalya97 <ashankaushalya952@gmail.com>
2020-02-12 12:25:46 +0530

    Add rerender state in delete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import Form from "./form"
+import { render } from '@testing-library/react';
+import { renderIntoDocument } from 'react-dom/test-utils';
 
 // function App() {
 //   const [todo,setValue] = useState([]);
@@ -41,7 +43,15 @@ function App(){
       // alert(event.target.value)
       event.preventDefault();
       console.log(list);
-      
+    }
+
+    function removeValue(item){
+      // alert("Clicked")
+      console.log(item);
+      const newlist = list.splice(item,1);
+      console.log(newlist);
+      console.log(list)
+      setValue({list:list})
     }
     
     // function showValues(event){
@@ -59,11 +69,14 @@ function App(){
         <input placeholder="Enter a value" onChange={handleChange}/>
         <input type="submit" value="submit" onClick={handleList}/>
         <input type="reset" value="Reset"/>
+        <input type="button" value="Remove All" onClick={()=>(setList([]))}/>
+
       </form>
   <div>
       <ul>
+        
         {list.map((item) => {
-          return <li key={list.indexOf(item.val)}>{item.val}</li>;
+          return <li key={list.indexOf(item)}>{item.val} <input type="button" value="Delete" onClick={()=> removeValue(list.indexOf(item))}/></li>;
         })}
       </ul>
 
